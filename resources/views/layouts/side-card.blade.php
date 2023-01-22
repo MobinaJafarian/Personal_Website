e8c3ec072e4326ff01ada7a74de9f955d8d3e391
Mobina <nimobina99@gmail.com>
2023-01-22 16:08:56 +0330

    update side-card

--- resources/views/layouts/side-card.blade.php
+++ resources/views/layouts/side-card.blade.php
@@ -9,7 +9,7 @@
         <li><a href="{{ route('home') }}">Dashboard</a></li>
         <li><a href="{{ route('home.index') }}">home</a></li>
         <li><a href="{{ route('about.index') }}">about me</a></li>
-        <li><a href="">my skills</a></li>
+        <li><a href="{{ route('skill.index') }}">my skills</a></li>
         <li><a href="">social</a></li>
         <li><a href="">blogs </a></li>
     </ul>
