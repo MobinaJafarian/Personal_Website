38f972258445f11baa4e915cb9aa12f99d089f34
Mobina <nimobina99@gmail.com>
2023-01-22 16:09:29 +0330

    add SkillController

---
+++ app/Http/Controllers/Admin/SkillController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SkillRequest;
+use App\Models\Skill;
+use Illuminate\Http\Request;
+
+class SkillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $skills = Skill::all();
+        return view('admin.skill.index' , compact('skills'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.skill.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(SkillRequest $request)
+    {
+        $inputs = $request->all();
+
+        $skill = Skill::create($inputs);
+        return redirect()->route('skill.index')->with('swal-success', 'Your skill page has been successfully launched');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $skill = Skill::findOrFail($id);
+
+        return view('admin.skill.edit' , compact('skill'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(SkillRequest $request, $id)
+    {
+        $skill = Skill::findOrFail($id);
+        $inputs = $request->all();
+        $skill->update($inputs);
+        return redirect()->route('skill.index')->with('swal-success', 'Your skill page has been successfully edited');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $skill = Skill::findOrFail($id);
+        $result = $skill->delete();
+        
+        return redirect()->route('skill.index')->with('swal-success', 'Your skill page has been successfully deleted');
+
+    }
+}
