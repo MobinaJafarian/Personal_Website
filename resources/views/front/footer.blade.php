05c7c91495054bc3abf540873087264ca5fed3cb
Mobina <nimobina99@gmail.com>
2023-01-23 10:10:22 +0330

    update footer

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="bg-dark py-5 mt-5">
-    <p class="m-0 text-white text-center">طراحی شده توسط خودم و خودم همینه که هست :|</p>
+    <p class="m-0 text-white text-center">Made By Me</p>
 </footer>
