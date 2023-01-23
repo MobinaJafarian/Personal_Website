bd300b424fdd940d2d61b52411e12f9dcb47920f
Mobina <nimobina99@gmail.com>
2023-01-23 09:34:46 +0330

    update front controller

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use App\Models\About;
+use App\Models\Blog;
 use App\Models\Home;
 use App\Models\Skill;
 use Illuminate\Http\Request;
@@ -16,7 +17,7 @@ class FrontController extends Controller
         $home = Home::orderBy('id' , 'desc')->first();
         $about = About::orderBy('id' , 'desc')->first();
         $skills = Skill::orderBy('id', 'asc')->take(10)->get();
-        // $blog = Blog::orderBy('id', 'desc')->take(3)->get();
-        return view('front.index' , compact('home' , 'about' , 'skills'));
+        $blog = Blog::orderBy('id', 'desc')->take(3)->get();
+        return view('front.index' , compact('home' , 'about' , 'skills' , 'blog'));
     }
 }
