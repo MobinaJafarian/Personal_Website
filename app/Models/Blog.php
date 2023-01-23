33784a02c3fc6c34a1dfd9db6f17b63fa5235a8b
Mobina <nimobina99@gmail.com>
2023-01-23 09:33:30 +0330

    add Blod model & migration

---
+++ app/Models/Blog.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Blog extends Model
+{
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+    ];
+}
