eeea6cea9cb16cba9a160f9bf351baeb404c9b2e
Mobina <nimobina99@gmail.com>
2023-01-23 09:35:51 +0330

    update  blogs blade

--- resources/views/front/blogs.blade.php
+++ resources/views/front/blogs.blade.php
@@ -1,24 +1,23 @@
 <section class="blogs py-5 mt-5">
     <div class="blog-title text-center py-5 mt-5">
-         <h2 class="fw-bold">بلاگ های من</h2>
-         <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
+         <h2 class="fw-bold">My Blogs</h2>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis unde, iure quod delectus itaque sequi nesciunt natus facilis? Voluptates magnam dolorem possimus minus cupiditate harum atque doloremque vitae et ratione.</p>
     </div>
 
     <div class="container">
          <div class="row">
-            {{-- @foreach ($blog as  $item)
+            @foreach ($blog as  $item)
             <div class="col-lg-4">
                 <div class="blog-item shadow">
-                <img src="{{ asset('admin/images/blogs/'.$item->image) }}" class="w-100" alt="">
+                <img src="{{ asset($item->image) }}" class="w-100" alt="">
                 <div class="blog-item-text p-2">
                      <h4 class="p-2 fw-bold fs-5 border-bottom">{{ $item->title }}</h4>
                      <p class="text-muted">{{ \Illuminate\Support\Str::limit($item->description, 100) }} </p>
-                     <a href="{{ route('blog.detail', $item->id) }}" target="_blank" class="btn btn-danger w-100">بخوانید</a>
+                     <a href="{{ route('blog.detail', $item->id) }}" target="_blank" class="btn btn-danger w-100">Read More</a>
                 </div>
                 </div>
            </div>
-            @endforeach --}}
-
+            @endforeach
          </div>
     </div>
 </section>
