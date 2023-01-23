f6e7303f5bca8ccda3055b84e7e4da1758b015df
Mobina <nimobina99@gmail.com>
2023-01-23 10:09:49 +0330

    add single page

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -7,17 +7,22 @@ use App\Models\About;
 use App\Models\Blog;
 use App\Models\Home;
 use App\Models\Skill;
-use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
 
     public function index()
     {
-        $home = Home::orderBy('id' , 'desc')->first();
-        $about = About::orderBy('id' , 'desc')->first();
+        $home = Home::orderBy('id', 'desc')->first();
+        $about = About::orderBy('id', 'desc')->first();
         $skills = Skill::orderBy('id', 'asc')->take(10)->get();
         $blog = Blog::orderBy('id', 'desc')->take(3)->get();
-        return view('front.index' , compact('home' , 'about' , 'skills' , 'blog'));
+        return view('front.index', compact('home', 'about', 'skills', 'blog'));
+    }
+
+    public function blogDetail($id)
+    {
+        $blog = Blog::findOrFail($id);
+        return view('front.single' , compact('blog'));
     }
 }
--- resources/views/front/single.blade.php
+++ resources/views/front/single.blade.php
@@ -20,13 +20,13 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-10">
-                    {{-- <div class="blog-item ">
-                        <img src="{{ asset('admin/images/blogs/'.$blog->image) }}" class="w-100" alt="">
+                    <div class="blog-item ">
+                        <img src="{{ asset($blog->image) }}" class="w-100" alt="">
                         <div class="blog-item-text p-2">
                             <h4 class="p-2 pb-4 fw-bold fs-5 border-bottom">{{ $blog->title }}</h4>
                             <p class="text-muted mt-5">{{ $blog->description }} </p>
                         </div>
-                    </div> --}}
+                    </div>
                 </div>
 
             </div>
