c7665cf92717eaac8c90abc98be698b7153ed1d8
Mobina <nimobina99@gmail.com>
2023-01-25 19:50:06 +0330

    update navbar style

--- resources/views/front/nav.blade.php
+++ resources/views/front/nav.blade.php
@@ -4,16 +4,19 @@
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">خانه</a>
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
                    </li>
                    <li class="nav-item">
-                        <a class="nav-link" href="#">بلاگ ها</a>
+                        <a class="nav-link" href="#">My skills</a>
+                   </li>
+                   <li class="nav-item">
+                        <a class="nav-link" href="#">My Blogs</a>
                    </li>
               </ul>
 
          </div>
 
-         <a class="navbar-brand" href="#">TopLearn</a>
+         <a class="navbar-brand" href="#">Personal Website</a>
 
     </div>
 </nav>
