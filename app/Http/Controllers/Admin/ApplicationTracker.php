84392f3e63750dfe5410dbfb1bb7f49c2c7e87f6
mobinajafarian <nimobina99@gmail.com>
2023-05-25 16:27:31 +0330

    updare appTrack controller

--- app/Http/Controllers/Admin/ApplicationTracker.php
+++ app/Http/Controllers/Admin/ApplicationTracker.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\ApplicationStatus;
 use App\Http\Controllers\Controller;
+use App\Models\ApplicationTracker as ModelsApplicationTracker;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Enum;
 
 class ApplicationTracker extends Controller
 {
@@ -14,7 +18,8 @@ class ApplicationTracker extends Controller
      */
     public function index()
     {
-        //
+        $applications = ModelsApplicationTracker::all();
+        return view('admin.applicationTracker.index', compact('applications'));
     }
 
     /**
@@ -24,7 +29,8 @@ class ApplicationTracker extends Controller
      */
     public function create()
     {
-        //
+        $applicationStatus = ApplicationStatus::cases();
+        return view('admin.applicationTracker.create', compact('applicationStatus'));
     }
 
     /**
@@ -35,7 +41,43 @@ class ApplicationTracker extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $inputs = $request->all();
+        // $application = ModelsApplicationTracker::create($inputs);
+
+
+        $request->validate([
+            'company_name' => ['required'],
+            'job_title' => ['required'],
+            'salary' => ['required'],
+            'location' => ['required'],
+            // 'link' => ['required'],
+            // 'note' => ['required'],
+            // 'todo' => ['required'],
+            'status' => 'required|in:wishlist,wantToApply,applied,interview,offer,rejected'
+            
+        ]);
+
+
+        $application = ModelsApplicationTracker::create([
+            'title' => $request->title,
+            'company_name' => $request->company_name,
+            'job_title' => $request->job_title,
+            'salary' => $request->salary,
+            'location' => $request->location,
+            'link' => $request->link,
+            'note' => $request->note,
+            'todo' => $request->todo,
+            // 'file' => $request->file,
+            'status' => $request->status,
+        ]);
+
+        return redirect()->route('applicationTracker.index');
+
+
+        
+
+
+
     }
 
     /**
@@ -57,7 +99,8 @@ class ApplicationTracker extends Controller
      */
     public function edit($id)
     {
-        //
+        $application = ModelsApplicationTracker::findOrFail($id);
+        return view('admin.applicationTracker.edit', compact('application'));
     }
 
     /**
@@ -80,6 +123,9 @@ class ApplicationTracker extends Controller
      */
     public function destroy($id)
     {
-        //
+        $application = ModelsApplicationTracker::findOrFail($id);
+        $result = $application->delete();
+
+        return redirect()->route('applicationTracker.index')->with('swal-success', 'Your blog has been successfully deleted');
     }
 }
